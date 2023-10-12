621df8c1ba688debb5cf29a18238c93d5eeb1e73
tienpd <tienpd@hybrid-technologies.vn>
2023-10-12 17:30:43 +0700

    test slider home

--- app/home/tests.py
+++ app/home/tests.py
@@ -1,6 +1,19 @@
 from django.test import TestCase
+from slider.models import Slider
 
 class HomeTestCase(TestCase):
+    def setUp(self):
+        Slider.objects.create(title='iPhone 6 Plus Dual SIM', url='img/h4-slide.png')
+        Slider.objects.create(title='by one, get one 50% off school supplies & backpacks.*', url='img/h4-slide2.png')
+        Slider.objects.create(title='Apple Store Ipod Select Item', url='img/h4-slide3.png')
+        Slider.objects.create(title='Apple Store Ipod & Phone', url='img/h4-slide4.png')
+        Slider.objects.create(title='Iphone15', url='img/h4-slide4.png')
     def test_can_access_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+        self.assertEqual(len(response.context['slider_list']), 4)
+        self.assertEqual(response.context['slider_list'][0].title, 'Iphone15')
+        self.assertEqual(response.context['slider_list'][1].title, 'Apple Store Ipod & Phone')
+        self.assertEqual(response.context['slider_list'][2].title, 'Apple Store Ipod Select Item')
+        self.assertEqual(response.context['slider_list'][3].title, 'by one, get one 50% off school supplies & backpacks.*')
--- app/home/views.py
+++ app/home/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from slider.models import Slider
 
 
 class HomePageView(TemplateView):
@@ -6,5 +7,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        slider_list = Slider.objects.order_by('-id').all()[:4]
         context["name"] = "world"
+        context["slider_list"] = slider_list
         return context

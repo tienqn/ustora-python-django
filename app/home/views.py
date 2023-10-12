74bb366dead0041f6a85774ac66b5a18fbcae9f8
tienpd <tienpd@hybrid-technologies.vn>
2023-10-12 14:55:03 +0700

    implement home

--- app/home/views.py
+++ app/home/views.py
@@ -6,5 +6,5 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["name"] = "index"
+        context["name"] = "world"
         return context
